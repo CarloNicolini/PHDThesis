4938252df4d588d3a3eaaf54d1343d77936eb76b
Carlo Nicolini <carlo.nicolini@iit.it>
2016-10-18 17:41:47 +0200

    removing uselesss things

--- continuos_build.sh
+++ continuos_build.sh
@@ -1,2 +1,2 @@
-when-changed chapters/ -c latexrun main.tex && latexpand main.tex > main_`date '+%b_%d_%G_%H_%M_%S'`.docx 
+when-changed chapters/ biblio.bib standalonetikz/ -c latexrun main.tex && latexpand main.tex > main_`date '+%b_%d_%G_%H_%M_%S'`.docx 
 
