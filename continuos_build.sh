b78098e96095c2b675eb8e35441aaee27f301d66
CarloNicolini <carlo.nicolini@iit.it>
2016-10-22 17:33:22 +0200

    writing second chapter

--- continuos_build.sh
+++ continuos_build.sh
@@ -1,2 +1,3 @@
-when-changed chapters/ biblio.bib standalonetikz/ -c latexrun main.tex && latexpand main.tex > main_`date '+%b_%d_%G_%H_%M_%S'`.docx 
+#when-changed chapters/ biblio.bib standalonetikz/ -c latexrun main.tex && latexpand main.tex > main_`date '+%b_%d_%G_%H_%M_%S'`.docx 
 
+when-changed chapters/ biblio.bib main.tex standalonetikz/ -c "say \"START!\" && cd build/ && make && cd .. && say \"FINISHED\""
