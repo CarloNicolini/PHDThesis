8a3e22c8b358492a440c1439c5d08239974e7dfa
Carlo Nicolini <carlo.nicolini@iit.it>
2016-11-25 16:36:17 +0100

    ...

--- runlatex.sh
+++ runlatex.sh
@@ -1,5 +1,5 @@
 #say "START!"
 clear && latexrun --latex-cmd=pdflatex --latex-args="-synctex=1" main.tex
-latexpand main.tex > main_`date '+%b_%d_%G_%H_%M_%S'`.docx
+#latexpand main.tex > main_`date '+%b_%d_%G_%H_%M_%S'`.docx
 cp latex.out/main.synctex.gz .
 #say "FINISHED!"
