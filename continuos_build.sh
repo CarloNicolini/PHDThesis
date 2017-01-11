5645d02fb5c9f9d0b46081e7044c2958d4f74792
Carlo Nicolini <carlo.nicolini@iit.it>
2017-01-11 18:15:51 +0100

    ...

--- continuos_build.sh
+++ continuos_build.sh
@@ -1,3 +1,3 @@
 when-changed chapters/*.tex biblio.bib main.tex -c "./runlatex.sh"
-
+when-changed schema_capitoli.dot -c "dot -Tpdf  schema_capitoli.dot >  schema_capitoli.pdf"
 #when-changed chapters/ biblio.bib main.tex standalonetikz/ -c "say \"START!\" && cd build/ && make && cd .. && say \"FINISHED\""
