7616ad8e8d3e66a406351c9f1ea15c466f132c50
Carlo Nicolini <carlo.nicolini@iit.it>
2017-02-03 09:49:53 +0100

    ...

--- runlatex.sh
+++ runlatex.sh
@@ -3,6 +3,6 @@ clear && latexrun --latex-cmd=pdflatex --latex-args="-synctex=1" main.tex
 #latexrun --latex-cmd=pdflatex --latex-args="-synctex=1" latex.out/main-frn.tex
 #latexrun --latex-cmd=pdflatex --latex-args="-synctex=1" main.tex
 #latexpand main.tex > main_`date '+%b_%d_%G_%H_%M_%S'`.docx
-#latexpand main.tex | grep -v -E "^\\\[a-z]" > mainexpand.tex
+latexpand main.tex > mainexpand.tex
 cp latex.out/main.synctex.gz .
 #say "FINISHED!"
