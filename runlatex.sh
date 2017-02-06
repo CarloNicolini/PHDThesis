cdd9591b51f1e96d610fd20f7d5b210fcfeac6c0
Carlo Nicolini <carlo.nicolini@iit.it>
2017-02-06 12:10:05 +0100

    ...

--- runlatex.sh
+++ runlatex.sh
@@ -4,7 +4,7 @@ cp latex.out/main.synctex.gz .
 #latexrun --latex-cmd=pdflatex --latex-args="-synctex=1" latex.out/main-frn.tex
 #latexrun --latex-cmd=pdflatex --latex-args="-synctex=1" main.tex
 #latexpand main.tex > main_`date '+%b_%d_%G_%H_%M_%S'`.docx
-# latexpand main.tex > mainexpand.tex
+#latexpand main.tex > mainexpand.tex
 
 # Check https://github.com/rkdarst/git-latexdiff
 # git latexdiff mainexpand.tex
